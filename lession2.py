1fe20afd17863b24703ede94db1042beba8fce08
chenxiao555 <18368111025@126.com>
2018-11-01 16:29:56 +0800

    third commit

--- lession2.py
+++ lession2.py
@@ -2,17 +2,26 @@
 for i in range(1,10):
     for j in range(1,10):
         if i>=j:
-            print('%s×%s=%s'%(i,j,i*j),end=' ')
+            print('%s×%s=%-2d'%(i,j,i*j),end=' ')
     print()
 
 for i in range(1,10):
     for j in range(1,10):
-        print('%s×%s=%s'%(i,j,i*j),end=' ')
+        print('%s×%s=%-2d'%(i,j,i*j),end=' ')
         if i<=j:
             break
     print()
 
 for i in range(1,10):
     for j in range(1,i+1):
-        print('%s×%s=%s'%(i,j,i*j),end=' ')
+        print('%s×%s=%-2d'%(i,j,i*j),end=' ')
     print()
+
+i = 9
+while i >= 1:
+    j = 1
+    while j <= i:
+        print('%d×%d=%-2d'%(i,j,j*i),end=' ')
+        j += 1
+    print()
+    i -= 1
